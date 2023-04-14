5c16996d02f6b0d4641aec2992ee64a0b1d5589b
Yves / Stan <48611390+stanlee17@users.noreply.github.com>
2023-04-14 23:25:28 +1000

    why choose us section added & styled

---
+++ src/components/Home/WhyUs.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+// Import images
+import Finance from '../../assets/home/desktop/icon-personal-finances.svg';
+import Banking from '../../assets/home/desktop/icon-banking-and-coverage.svg';
+import Payments from '../../assets/home/desktop/icon-consumer-payments.svg';
+
+const WhyUs = () => {
+  return (
+    <div>
+      <div className="container mx-auto my-48 flex leading-loose">
+        <div className="flex flex-col items-center text-center mr-16">
+          <img src={Finance} alt="personal-finances" className="mb-8" />
+          <h4 className="mb-3 font-bold text-lg text-sanJuanBlue">
+            Personal Finances
+          </h4>
+          <p className="text-lightSanJuanBlue">
+            Consolidate financial data from multiple sources and categorize
+            transactions up to 2 years of history. Analyze reports to reconcile
+            activities in your account.
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center mr-16">
+          <img src={Banking} alt="banking-coverage" className="mb-8" />
+          <h4 className="mb-3 font-bold text-lg text-sanJuanBlue">
+            Banking & Coverage
+          </h4>
+          <p className="text-lightSanJuanBlue">
+            With our platform, you can speed up account onboarding and support
+            ongoing payments for checking, savings, credit card, and brokerage
+            accounts.
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center mr-16">
+          <img src={Payments} alt="consumer-payments" className="mb-8" />
+          <h4 className="mb-3 font-bold text-lg text-sanJuanBlue">
+            Consumer Payments
+          </h4>
+          <p className="text-lightSanJuanBlue">
+            It’s easier to set up secure bank payments with us through a flow
+            designed with the user experience in mind. Customers could instantly
+            authenticate their account.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyUs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Hero from '../components/Home/Hero';
 import Companies from '../components/Home/Companies';
 import Product from '../components/Home/Product';
+import WhyUs from '../components/Home/WhyUs';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <Companies />
       <Product />
+      <WhyUs />
     </div>
   );
 };
